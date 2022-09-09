d483cdd7aa00a29efc978f85d4429c373d1eec14
wambuiwambui <tnjoki11@gmail.com>
2022-09-09 21:39:25 +0000

    git commit -m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char x;
+	for(x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
