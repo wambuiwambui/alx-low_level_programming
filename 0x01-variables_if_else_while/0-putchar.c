2183373502a85290e40d4a180db3b0593e95d5da
wambuiwambui <tnjoki11@gmail.com>
2022-09-12 08:42:04 +0000

    putchar

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar, followed by a new line
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	{
+		printf('_putchar');
+	}
+	printf('/n')
+	return (0);
+{
+
+
+
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Return: 0 (success)
+*/
+{
+	switch(printf("_putchar\n"))
+	return 0;
+}
