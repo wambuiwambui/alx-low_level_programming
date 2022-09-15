4fe6d4cf1b8c17b2496e4020442c27ec16d6f708
wambuiwambui <tnjoki11@gmail.com>
2022-09-15 15:04:17 +0000

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
-*main - header file for prototypes
-*void prototypes
-*int prototypes(int)
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
 */
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -19,4 +19,6 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 int _putchar(char);
+
+#endif /* MAIN_H */
 
