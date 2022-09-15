81b542447f08b987cba98a9ae52b76bfb7421df6
wambuiwambui <tnjoki11@gmail.com>
2022-09-15 17:45:48 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
-
 /**
- * main - main block
- * Description: Find and print the first 98 fib numbers starting with 1 and 2.
- * Numbers should be coma and space separated.
- * Return: 0
+*main - prints out first 98
+*fibonacci suit numbers
+*Return: return 0
 */
 int main(void)
 {
-	int i;
-	unsigned long n1 = 1, n2 = 2, n3;
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+unsigned long h1, h2;
 
-	printf("%lu, %lu", n1, n2);
+for (inc = 0; inc < 92; inc++)
+{
+n3 = n1 + n2;
+printf("%lu, ", n3);
+n1 = n2;
+n2 = n3;
+}
+n1_h1 = n1 / 10000000000;
+n2_h1 = n2 / 10000000000;
+n1_h2 = n1 % 10000000000;
+n2_h2 = n2 % 10000000000;
+for (inc = 93; inc < 99; inc++)
+{
+h1 = n1_h1 + n2_h1;
+h2 = n1_h2 + n2_h2;
+if ((n1_h2 + n2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (inc != 98)
+printf(", ");
 
-	for (i = 0; i < 96; i++)
-	{
-		n3 = n1 + n2;
-		printf("%lu", n3);
-		if (i != 95)
-		{
-			printf(", ");
-		}
-		else
-		{
-			printf("\n");
-		}
-		n1 = n2;
-		n2 = n3;
-	}
-	return (0);
+n1_h1 = n2_h1;
+n1_h2 = n2_h2;
+n2_h1 = h1;
+n2_h2 = h2;
+}
+printf("\n");
+return (0);
 }
