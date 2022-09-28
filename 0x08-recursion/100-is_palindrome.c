2d9dfc9db34b42adf6878430d9f3763fffec0c23
wambuiwambui <tnjoki11@gmail.com>
2022-09-28 21:26:35 +0000

    task 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -34,3 +34,20 @@ void check(char *s, int first, int end, int *tag)
 	}
 }
 
+/**
+ * _strlen_recursion - calculates length of a string
+ * @s: string
+ *
+ * Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
