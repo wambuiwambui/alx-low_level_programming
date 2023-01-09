61db79893a63646214a37f660679ec132ca0cdb1
Tabitha Njoki <tnjoki11@gmail.com>
2023-01-09 22:39:46 +0300

    Remove wrong folder

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This module defines the is_same_class function"""
-
-
-def is_same_class(obj, a_class):
-    """returns True or False based on inheritance"""
-    return type(obj) == a_class
