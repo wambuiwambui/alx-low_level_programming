a01f3affb2f12f6000f0d086650fb880a06534c0
wambuiwambui <tnjoki11@gmail.com>
2023-04-16 20:57:00 +0300

    Initial commit

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,39 +1,53 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
-  * jump_search - Searches for a value in a sorted array
-  *               of integers using jump search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array.
-  *              Uses the square root of the array size as the jump step.
-  */
+ * jump_search - Searches for a value in a sorted array of integers
+ *               using the Jump search algorithm
+ * @array: A pointer to the first element of the array to search
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located, or -1 if not found or
+ *         array is NULL
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, jump, step;
+    size_t step = sqrt(size);
+    size_t prev = 0;
 
-	if (array == NULL || size == 0)
-		return (-1);
+    if (array == NULL)
+        return (-1);
 
-	step = sqrt(size);
-	for (i = jump = 0; jump < size && array[jump] < value;)
-	{
-		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-		i = jump;
-		jump += step;
-	}
+    while (array[prev] < value)
+    {
+        size_t curr = fmin(size - 1, prev + step);
 
-	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+        if (array[curr] >= value)
+        {
+            printf("Value found between indexes [%ld] and [%ld]\n", prev, curr);
+            break;
+        }
 
-	jump = jump < size - 1 ? jump : size - 1;
-	for (; i < jump && array[i] < value; i++)
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        prev = curr;
+        printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 
-	return (array[i] == value ? (int)i : -1);
+        if (prev == size - 1)
+            return (-1);
+    }
+
+    while (array[prev] < value)
+    {
+        prev++;
+        if (prev == fmin(size, prev + step))
+            return (-1);
+    }
+
+    if (array[prev] == value)
+    {
+        printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+        return (prev);
+    }
+
+    return (-1);
 }
--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,47 +1,44 @@
-/*
- * File: 102-interpolation.c
- * Author: zeddymuge
- */
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
-  * interpolation_search - Searches for a value in a sorted array
-  *                        of integers using interpolation search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array..
-  */
+ * interpolation_search - searches for a value in a sorted array of integers
+ * using the interpolation search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located, or -1 on failure
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t i, l, r;
+	size_t low = 0, high = size - 1, pos;
 
-	if (array == NULL)
+	if (!array || size == 0)
 		return (-1);
 
-	for (l = 0, r = size - 1; r >= l;)
+	while (low <= high)
 	{
-		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-		if (i < size)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		else
+		pos = low + (((double)(high - low) /
+			      (array[high] - array[low])) * (value - array[low]));
+
+		if (pos < low || pos > high)
 		{
-			printf("Value checked array[%ld] is out of range\n", i);
+			printf("Value checked array[%lu] is out of range\n", pos);
 			break;
 		}
 
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			r = i - 1;
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
 		else
-			l = i + 1;
+			high = pos - 1;
 	}
 
 	return (-1);
 }
+
